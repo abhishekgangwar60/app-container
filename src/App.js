8f2cdf8001cc17cbba2568fe737821b5dfbd6792
Abhishek Gangwar <Abhishek.Gangwar@gartner.com>
2020-05-11 15:16:50 +0530

    Setup Project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import MicroFrontEndOne from "micro-front-end-one";
+import MicroFrontEndTwo from "micro-front-end-two";
 
 function App() {
   return (
     <div className="App">
       This is app container.
       <MicroFrontEndOne />
+      <MicroFrontEndTwo />
     </div>
   );
 }
